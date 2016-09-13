c37186b4dcf14ad3d3cd99043d276e9137d023d0
Dean Gardiner <me@dgardiner.net>
2016-09-13 22:57:26 +1200

    Updated `Task` class

--- trakt_tools/tasks/core/base.py
+++ trakt_tools/tasks/core/base.py
@@ -1,22 +1,2 @@
-from trakt import Trakt
-
-
 class Task(object):
-    def __init__(self):
-        self._settings = None
-
-    def get_settings(self):
-        if self._settings is None:
-            # Retrieve account settings
-            self._settings = Trakt['users/settings'].get()
-
-        # Return cached settings
-        return self._settings
-
-    def get_username(self):
-        settings = self.get_settings()
-
-        if not settings:
-            return None
-
-        return settings.get('user', {}).get('username')
+    pass
