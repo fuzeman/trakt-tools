538daf68f3104f3250d2d1ef2ce7856f19e38ea3
Dean Gardiner <me@dgardiner.net>
2016-09-17 15:49:19 +1200

    Switched [setup.py] to pbr

--- setup.py
+++ setup.py
@@ -1,34 +1,7 @@
-from setuptools import setup, find_packages
-import re
+from setuptools import setup
 
-
-version = re.search(
-    '^__version__\s*=\s*["\'](.*)["\']',
-    open('trakt_tools/version.py').read(),
-    re.M
-).group(1)
-
-with open("README.rst", "rb") as f:
-    long_description = f.read().decode("utf-8")
 
 setup(
-    name="trakt-tools",
-    version=version,
-
-    description='Command-line tools for Trakt.tv',
-    long_description=long_description,
-
-    author='Dean Gardiner',
-    author_email='me@dgardiner.net',
-
-    packages=find_packages(include=[
-        'trakt_tools',
-        'trakt_tools.*'
-    ]),
-
-    entry_points={
-        'console_scripts': [
-            'trakt_tools = trakt_tools.runner.main:main'
-        ]
-    }
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
